a0503dd05940a4a4aa985155bce112310f5fbd25
Soooobiniya <rorngk22@gmail.com>
2023-12-08 11:07:52 +0900

    feat: 페이지 이동 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_flutter/first_page.dart';
-import 'package:my_flutter/second_page.dart';
 
 void main() => runApp(MyApp());
-
 
 class MyApp extends StatelessWidget {
   @override
@@ -11,14 +8,105 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange
       ),
-      home: FirstPage(),
-      routes: {
-        '/first': (context) => FirstPage(),
-        '/second': (context) => SecondPage(),
-      },
+      home: MyHomePage(),
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _index = 0; // 페이지 인덱스 0, 1, 2
+  var _pages = [
+    Page1(),
+    Page2(),
+    Page3(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pinkAccent,
+        title: Text(
+        'Flitter Custom UI',
+        style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.add,
+                color: Colors.white,
+              )
+          )
+        ],
+      ),
+      body: _pages[_index],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            _index = index; // 선택된 탭의 인덱스로 _index를 변경
+          });
+        },
+        currentIndex: _index, // 선택된 인덱스
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              label: '홈',
+              icon: Icon(Icons.home)
+          ),
+          BottomNavigationBarItem(
+              label: '이용서비스',
+              icon: Icon(Icons.assignment)
+          ),
+          BottomNavigationBarItem(
+              label: '내 정보',
+              icon: Icon(Icons.account_circle)
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        '홈 페이지',
+        style: TextStyle(fontSize: 40),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        '이용서비스',
+        style: TextStyle(fontSize: 40),
+      ),
+    );
+  }
+}
+
+class Page3 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        '내 정보',
+        style: TextStyle(fontSize: 40),
+      ),
+    );
+  }
+}
